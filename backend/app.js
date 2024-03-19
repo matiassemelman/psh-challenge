d9e837acadf3eef0fb5a2552023e674807c29615
Matias <matias.semelman@protonmail.com>
2024-03-19 16:37:03 -0300

    Add playerStatRoutes and Sequelize configuration

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,39 @@
 const express = require("express");
+const playerStatRoutes = require("./routes/playerStatRoutes");
+const { Sequelize } = require("sequelize");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    await sequelize.sync(); // Sincroniza los modelos con la base de datos
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
+
+app.use("/", playerStatRoutes);
+
+// Default route handler
+app.get("*", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// Start the server
+startServer();
