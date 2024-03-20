b4f67b562281df05980323738f89502c6283b049
Matias <matias.semelman@protonmail.com>
2024-03-20 16:22:39 -0300

    Refactor utils.ts and add fetchPlayers function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Player } from "@/types/PlayerType";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function fetchPlayers(setPlayers: (players: Player[]) => void) {
+  return fetch("http://localhost:3000/api/players/top")
+    .then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+        setPlayers(data);
+      })
+    )
+
+    .catch((error) => {
+      console.error("Error fetching top players:", error);
+    });
 }
