b145a5faa9446482ce3a302f54cf4aa0e5e66814
Matias <matias.semelman@protonmail.com>
2024-03-20 18:13:16 -0300

    Add CSV download functionality to Button component

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,5 +1,11 @@
+import { Player } from "@/types";
 import { Button } from "../ui/button";
+import { handleDownloadCSV } from "./utils";
 
-export const ButtonCSV = () => {
-  return <Button variant={"default"}>Download CSV</Button>;
+export const ButtonCSV = ({ players }: { players: Player[] }) => {
+  return (
+    <Button variant={"default"} onClick={() => handleDownloadCSV(players)}>
+      Download CSV
+    </Button>
+  );
 };
