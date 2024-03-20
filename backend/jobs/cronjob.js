2736b09d2510f767d107cabff5f963aef9af3a80
Matias <matias.semelman@protonmail.com>
2024-03-20 17:13:42 -0300

    Fix typo in function name

--- backend/jobs/cronjob.js
+++ backend/jobs/cronjob.js
@@ -1,11 +1,11 @@
 const cron = require("node-cron");
-const { generateRandomPlayerStat } = require("../services/playerStatService");
+const { generateRandomPlayerStats } = require("../services/playerStatService");
 
 // Definir la tarea programada
 const cronJob = cron.schedule("*/5 * * * *", async () => {
   try {
     // Llamar a la función para generar estadísticas aleatorias de jugadores
-    await generateRandomPlayerStat();
+    await generateRandomPlayerStats();
     console.log("Estadísticas de jugadores generadas exitosamente");
   } catch (error) {
     console.error("Error al generar estadísticas de jugadores:", error);
